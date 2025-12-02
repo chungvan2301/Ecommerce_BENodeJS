f646440773647b0616f6d3e3ed2b7def9ae418f6
Nguyễn Chung Văn <nguyenchungvan.t1.15cla@gmail.com>
2025-12-03 00:17:11 +0700

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -5,13 +5,13 @@ const asyncHandler = require('express-async-handler');
 //check người dùng vừa login
 const authMiddleware = asyncHandler(async(req,res,next)=>{
     let token;
-    if (req?.headers?.authorization?.startsWith('Bearer')){     //Kiểm tra xác thực 1 chuỗi bắt đầu bằng Bearer trong headers
-        token = req.headers.authorization.split(' ')[1];        //Lấy token 
+    if (req?.headers?.authorization?.startsWith('Bearer')){    
+        token = req.headers.authorization.split(' ')[1];      
         try {
-            const decode = await jwt.verify(token, process.env.JWT_SECRET)  //từ token decode để lấy ra _id
+            const decode = await jwt.verify(token, process.env.JWT_SECRET) 
             
             const user = await User.findById(decode.id);
-            req.user = user;                   //lưu user vừa tìm vào req.user
+            req.user = user;                   
             next();
         } catch (error) {
             throw new Error('Not authorized, please login again')
